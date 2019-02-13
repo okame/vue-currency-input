d1a35c201e6cff73500d074069c85953ec9a6025
okame <pikkoro888@gmail.com>
2019-02-13 21:33:16 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'vue',
+    'plugin:vue/essential',
+    'standard'
+  ],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-return-await': 'off'
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
 <template>
-  <input type="text" @blur="blur" v-model="tmp" :id="id" :name="name" />
+  <input type="text" @blur="blur" v-model="visibleValue" :id="id" :name="name" />
 </template>
 
 <script>
 export default {
-  name: 'moneyInput',
+  name: 'VueCurrencyInput',
 
-  props: [
-    'value',
-    'id',
-    'name'
-  ],
+  props: {
+    value: {
+      default: 0,
+      type: Number
+    },
+    id: {
+      default: null,
+      type: String
+    },
+    name: {
+      default: null,
+      type: String
+    }
+  },
 
   data () {
     return {
-      tmp: 0
+      visibleValue: 0
     }
   },
 
   mounted () {
-    this.tmp = this.value
+    this.visibleValue = this.value
     this.split()
   },
 
   computed: {
     normalizedValue () {
-      return parseInt(String(this.tmp || 0).replace(/,/g, ''), 10)
+      return parseInt(String(this.visibleValue || 0).replace(/,/g, ''), 10)
     }
   },
 
@@ -38,10 +47,10 @@ export default {
     },
     split () {
       this.normalize()
-      this.tmp = String(this.tmp).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
+      this.visibleValue = String(this.visibleValue).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
     },
     normalize () {
-      this.tmp = parseInt(String(this.tmp || 0).replace(/,/g, ''), 10)
+      this.visibleValue = parseInt(String(this.visibleValue || 0).replace(/,/g, ''), 10)
     },
     validate (value) {
       return /^[\d,]*$/.test(value)
@@ -50,12 +59,12 @@ export default {
 
   watch: {
     value () {
-      this.tmp = this.value
+      this.visibleValue = this.value
       this.blur()
     },
-    tmp (newValue, oldValue) {
+    visibleValue (newValue, oldValue) {
       if (!this.validate(newValue)) {
-        this.tmp = oldValue
+        this.visibleValue = oldValue
       } else {
         this.split()
         this.$emit('input', this.normalizedValue)
